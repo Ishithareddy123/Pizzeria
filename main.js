b05ac1f8141a5b01c443a5939cc8cdcad9d8e0a1
Ishithareddy123 <81242657+Ishithareddy123@users.noreply.github.com>
2021-08-31 17:22:42 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
 menu_list_array = ["Veg Margherita Pizza","Chees Pizza","Chicken Tandoori pizza","Olive Drizzle Pizza","Paprika spice pizza"]
 function getmenu(){
 var htmldata;
- htmldata="<'ol class="menulist'>";
+ htmldata="<ol class='menulist'>";
  menu_list_array.sort();
- for(var i=0;i<menu_list_array.length);i++){
+ for(var i=0;i<menu_list_array.length;i++){
  htmldata=htmldata+"<li>"+menu_list_array[i]+"</li>";
 //Complete the code
 }
+
 htmldata=htmldata+"</ol>"
 document.getElementById("display_menu").innerHTML=htmldata;
+}
 function add_item(){
 var htmldata;
 var item=document.getElementById("add_item").value;
 //Complete the code
 menu_list_array.sort();
-htmldata="<section class="card">"
+htmldata="<section class='card'>"
 htmldata=htmldata+"<li>"+menu_list_array[i]+"</li>"{
-    htmldata=htmldata+"<div class="card">";
+    htmldata=htmldata+"<div class='card'>";
     +"<img src=pizzalmg.png>"
     +menu_list_array[i]+"</div>"; 
 }
