de1ebc7f332062c45dc4c86706d26f0cfc797834
Ishithareddy123 <81242657+Ishithareddy123@users.noreply.github.com>
2021-09-12 11:03:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -12,22 +12,28 @@ htmldata=htmldata+"</ol>"
 document.getElementById("display_menu").innerHTML=htmldata;
 }
 function add_item(){
-var htmldata;
-var item=document.getElementById("add_item").value;
+var htmldata="";
+
 //Complete the code
 menu_list_array.sort();
-htmldata="<section class='card'>"
-htmldata=htmldata+"<li>"+menu_list_array[i]+"</li>"{
-    htmldata=htmldata+"<div class='card'>";
-    +"<img src=pizzalmg.png>"
+for( var i=0;i<menu_list_array.length;i++){
+
+
+htmldata="<section class='card'>";
+//htmldata=htmldata+"<li>"+menu_list_array[i]+"</li>"
+    htmldata=htmldata+"<div class='card'>"
+    +"<img src='pizzaImg.png'>"
     +menu_list_array[i]+"</div>"; 
-}
+
 htmldata=htmldata+"</section>";
+console.log(htmldata);
+}
 document.getElementById("display_addedmenu").innerHTML=htmldata;
+}
 function add_top(){
 //Complete the code
 var item=document.getElementById("add_item").value;
 menu_list_array.push(item);
-add_item();
+
 menu_list_array.sort();
 }
